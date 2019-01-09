bd706e40de08feac6dc2a22a031ba3eab49e96b7
Adrian (Lloyd) Flanagan <aflanagan@bhmginc.com>
2019-01-09 15:09:29 -0500

    Rename grammar file to 'grammar'

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,16 @@ Licensed under the MIT License, see file LICENSE-MIT.txt
 #[macro_use]
 extern crate lalrpop_util;
 
-lalrpop_mod!(pub calculator1);
+lalrpop_mod!(pub grammar);
 
 fn main() {
     println!("Hello, world!");
 }
 
 #[test]
-fn calculator1() {
-    assert!(calculator1::TermParser::new().parse("22").is_ok());
-    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
-    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
-    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+fn nlfind() {
+    assert!(grammar::TermParser::new().parse("22").is_ok());
+    assert!(grammar::TermParser::new().parse("(22)").is_ok());
+    assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
+    assert!(grammar::TermParser::new().parse("((22)").is_err());
 }
